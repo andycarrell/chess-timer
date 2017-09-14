4a2583063998b85cf758c023df980bfb307ca90e
Andy Carrell <carrellandrew@gmail.com>
2017-09-14 22:01:05 +1200

    Adds react native app using create-react-app

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles.js';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>dab</Text>
+      </View>
+    );
+  }
+}
