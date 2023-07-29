1110452d09d5304fd0499e1ed0a9ca0b4e19ec37
andycarrell <carrellandrew@gmail.com>
2023-07-29 18:23:40 +1200

    removes now unneeded old files

--- src/helpers/interval.js
+++
@@ -1,3 +0,0 @@
-const setIntervalAt = (seconds) => (fn) => setInterval(fn, seconds * 1000);
-
-export const everySecondRun = setIntervalAt(1);
--- src/helpers/setState.js
+++
@@ -1,9 +0,0 @@
-export const toggleFor = (key) => (prevState) => ({
-  ...prevState,
-  [key]: !prevState[key],
-});
-
-export const decrementFor = (key) => (prevState) => ({
-  ...prevState,
-  [key]: prevState[key] - 1,
-});
